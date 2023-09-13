a5be1a56d1751d50b62584cc29dfa23b3cffb6ad
ali <iamaliahad@outlook.com>
2023-09-13 14:56:31 +0600

    background change

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import { Line } from "@/components/manual/line";
+import Particles from "@/components/particles";
 import { PortfolioCard } from "@/components/portfolioCard";
 import Link from "next/link";
 export default function Home() {
   return (
     <>
+      <Particles
+        className="absolute inset-0 -z-10 animate-fade-in"
+        quantity={100}
+      />
       <div className="flex flex-row content-between gap-3 justify-center p-3">
         <div className="pt-4">
           <h1>Featured Portfolio</h1>
